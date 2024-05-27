cdca25203913db0598c8f717bc4decd9067180e3
epileptickeed <godjieva2013@yandex.ru>
2024-05-27 17:43:07 +0300

    fixed arc error

--- client/src/pages/Graph/Graph.tsx
+++ client/src/pages/Graph/Graph.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
-import { Pie } from 'react-chartjs-2';
-import { Chart as ChartJS } from 'chart.js/auto';
+import React from "react";
+import { Pie } from "react-chartjs-2";
+import { Chart as ChartJS } from "chart.js/auto";
+import { Chart, ArcElement } from "chart.js";
+Chart.register(ArcElement);
 
 const Graph = ({ chartData }: any) => {
   return (
